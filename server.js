dc78f4f8a77ae859aa794219e2356da63caf67db
Cesar Preza <cpreza93@yahoo.com>
2025-03-16 23:04:45 -0700

    removed unused code

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
-const port = process.env.PORT;
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -17,19 +16,15 @@ app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
-const testJwtRouter = require('./controllers/test-jwt');
 const authRouter = require('./controllers/auth');
 const usersRouter = require('./controllers/users');
 const consolesRouter = require('./controllers/consoles');
 const gamesRouter = require('./controllers/games');
-// Routes go here
-//CHANGE THESE ROUTES
 app.use('/auth', authRouter);
-// app.use('/test-jwt', testJwtRouter);
 app.use('/users', usersRouter);
 app.use('/consoles', consolesRouter);
 app.use('/games', gamesRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port 3000`);
 });
